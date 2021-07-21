c75c446bdff53898337579fa6771b26beb22247e
Taburashka <maxtabolin@gmail.com>
2021-07-21 12:32:04 +0000

    change dus-ml

--- utils.py
+++ utils.py
@@ -67,17 +67,25 @@ def round_to_int(x,**kwargs):
 
 def decomposition(out_y):
     pairs = {'AXBX':['AX','BX'],'BYCY':['BY','CY'],'CXDX':['CX','DX'],'DYAY':['DY','AY']}
-    directQL = {'Q': [['AQ','CQ'],['BL','DL']],'L':[['AL','CL'],['BQ','DQ']]}
+    directQL_part = {'Q': [['AQ','CQ'],['BL','DL']],'L':[['AL','CL'],['BQ','DQ']]}
+    directQL_full = {'Q': ['AQ','CQ','BL','DL'],'L':['AL','CL','BQ','DQ']}
     massQL = {'A': ['AQ','AL'],'B': ['BQ','BL'],'C': ['CQ','CL'], 'D': ['DQ','DL']}
     delete = {'del': [['X','Y'],['Q','L']]}
     out_y[['A','B','C','D','X','Y','Q','L','del']] = 0
-    
+
     out_y = add_pairs(out_y, **pairs) #from component mass(X,Y) to pairs (AXBX, CXDX, BYCY, DYAY)     
     out_y = out_y.apply(del_pairs, **pairs, axis = 1) # from pairs (AXBX,CXDX,BYCY,DYAY) to mass(A,B,C,D) and direction (X,Y)
     out_y = out_y.apply(backprop, **massQL, axis = 1) # from mass(A,B,C,D) to component mass(Q,l)
-    out_y = out_y.apply(forward, **directQL, axis = 1) # from component mass (Q,L) to direction (Q,L)
+    out_y = out_y.apply(forward, **directQL_part, axis = 1) # from component mass (Q,L) to direction (Q,L)
     out_y = out_y.apply(forward, **delete, axis = 1) # delete bias
-    out_y = out_y.drop(columns = ['AXBX','CXDX','BYCY','DYAY','A','B','C','D','del'])
+    # out_y['AQ'] = out_y['AQ'] - out_y['CQ']
+    # out_y['AL'] = out_y['AL'] - out_y['CL']
+    # out_y['BQ'] = out_y['BQ'] - out_y['DQ']
+    # out_y['BL'] = out_y['BL'] - out_y['DL']
+    # out_y['X'] = out_y['X'] - out_y['Y']
+    # out_y['L'] = out_y['L'] - out_y['Q']
+    out_y = out_y.apply(backprop, **directQL_full, axis = 1) # delete bias
+    out_y = out_y.drop(columns = ['AXBX','CXDX','BYCY','DYAY','A','B','C','D','Q','L','del'])
     return out_y
 
 def integration(out_y, nelem, koefXY = 1, koefQL = 1):
@@ -126,11 +134,15 @@ def create_out(len_out: int = 100):
     return in_y
 
 def add_phase(x, **kwargs):
+    max_value = 0
+    for key in kwargs.keys():
+        if max_value <  np.max(np.abs(x[kwargs[key]])):
+          max_value = np.max(np.abs(x[kwargs[key]]))
     for key in kwargs.keys():
         values = kwargs[key]
         dir1 = values[0]
         dir2 = values[1]
-        if x[dir2] == 0 or x[dir1] == 0:
+        if x[dir2] == 0 or x[dir1] == 0 or (np.abs(x[dir1]/max_value) < 0.2 and np.abs(x[dir2]/max_value) < 0.2):
             x[key] = 0.5
         elif (np.sign(x[dir1]) == np.sign(x[dir2])):
             x[key] = np.sin(np.arctan(x[dir2]/x[dir1]) * 1.33 + 0.523)
@@ -138,8 +150,7 @@ def add_phase(x, **kwargs):
             x[key] = 1 -  np.sin(np.arctan(np.abs(x[dir2]/x[dir1])) * 1.33 + 0.523)
     return x
 
-def init_in(X, freq_bias):  
-    nmode = len(freq_bias) 
+def init_in(X, nmode, old_consts):  
     ind1_list = []
     ind2_list = []
     for i in range(X.shape[0]):
@@ -148,6 +159,26 @@ def init_in(X, freq_bias):
     mInd = pd.MultiIndex.from_arrays([ind1_list, ind2_list], names=('number', 'mode'))
     X.index = mInd
     
+    xs = pd.IndexSlice
+    addX2 = X.loc[xs[:,2],:].droplevel(1)
+    addX3 = X.loc[xs[:,3],:].droplevel(1)
+    addX2['X'] = (addX2['AX'] + addX2['BX'] + addX2['CX'] + addX2['DX'])/4
+    addX2['Y'] = (addX2['AY'] + addX2['BY'] + addX2['CY'] + addX2['DY'])/4
+    addX3['X'] = (addX3['AX'] + addX3['BX'] + addX3['CX'] + addX3['DX'])/4
+    addX3['Y'] = (addX3['AY'] + addX3['BY'] + addX3['CY'] + addX3['DY'])/4
+    addX2.drop(columns = ['AX','BX','CX','DX','AY','BY','CY','DY'], inplace = True)
+    addX3.drop(columns = ['AX','BX','CX','DX','AY','BY','CY','DY'], inplace = True)
+    addX = pd.concat([addX2.freq, addX3.freq, np.sign(addX2.X) * (np.abs(addX2.X) + np.abs(addX3.Y))/2,
+                      np.sign(addX2.Y) * (np.abs(addX3.X) + np.abs(addX2.Y))/2], axis = 1)
+    addX.columns = ['freq2','freq3','X2','Y2']
+    phase = {'phase2':['X2','Y2']}
+    addX = addX.apply(add_phase, **phase, axis = 1)
+    addX = np.abs(addX)
+    maxval = np.max(addX[['X2','Y2']], axis = 1)
+    addX[['X2','Y2']] = addX[['X2','Y2']].divide(maxval, axis = 0, level=1)
+
+    X = X.drop(index=[2,3], level = 1)
+
     phase = {'phaseA':['AX','AY'],'phaseB':['BX','BY'],'phaseC':['CX','CY'],'phaseD':['DX','DY']}
     X = X.apply(add_phase, **phase, axis = 1)
     
@@ -158,36 +189,32 @@ def init_in(X, freq_bias):
     pairs_keys = ['AXBX','BYCY','CXDX','DYAY']
     maxval = np.max(X[pairs_keys], axis = 1)
     X[pairs_keys] = X[pairs_keys].divide(maxval, axis = 0, level=1)
-    X = X.round(3)
-    #for col_name in X.columns:
-    #  X[col_name][X[col_name] < 0.2] = 0
     
     #X  = X.groupby(level = 'number').apply(norm_freq, freq_bias)
-    
 
     new_X = pd.DataFrame()
     colX = X.columns
-    for idx, mode in enumerate(X.groupby(['mode'])):
-        col = {key: key + str(idx+1)  for key in colX}
-        new_X = pd.concat([new_X, pd.DataFrame(mode[1]).droplevel(1).rename(columns = col)] , axis = 1) 
+    for idx in X.groupby(['mode']).count().index:
+        col = {key: key + str(idx)  for key in colX}
+        new_X = pd.concat([new_X, X.loc[xs[:,idx],:].droplevel(1).rename(columns = col)] , axis = 1) 
     
-    for i in range(1, len(freq_bias) + 7):
-        new_X['freq'+ str(8 + i)] = new_X['freq'+ str(i + 1)] - new_X['freq'+ str(i)] 
+    new_X = pd.concat([new_X, addX] , axis = 1) 
+    for i in range(1, nmode):
+         new_X['freq'+ str(nmode + i)] = new_X['freq'+ str(i + 1)] - new_X['freq'+ str(i)] 
     
-    for i in range(1, len(freq_bias) + 7):
-        name = 'freq' + str(i + 1)
-        if i < 9:
-            new_X[name] = new_X[name] - new_X['freq1']
-        new_X[name] = (new_X[name] - new_X[name].min())/(new_X[name].max() - new_X[name].min())
-    new_X = new_X.drop(columns = ['freq1'])
+    new_consts = []
+    new_consts.append([new_X['freq1'].min(), new_X['freq1'].max()])
+    koef = new_X['freq1']/((old_consts[0][0]+ old_consts[0][1])/2)
+    for i in range(2, nmode + 7):
+        name = 'freq' + str(i+1)
+        if i < 8:
+            new_X[name] = new_X[name] - new_X['freq1']  
+        new_consts.append([new_X[name].min(), new_X[name].max()])
+        #new_X[name] = (new_X[name] - new_X[name].min())/(new_X[name].max() - new_X[name].min())
+        new_X[name] = ( koef * old_consts[i-1][1] - new_X[name])/(koef * old_consts[i-1][1] - koef * old_consts[i-1][0])
+    new_X = new_X.drop(columns = ['freq1','freq2','phaseA1','phaseB1','phaseC1','phaseD1','phaseA4','phaseB4','phaseC4','phaseD4'])
     new_X = new_X.round(3)
-    #new_X = new_X.drop(columns = ['freq1','freq4'])
-#     if (nmode == 8):
-#         directXY = {'X2': ['AXBX2','CXDX2'],'Y2': ['BYCY2','DYAY2'],'X3': ['AXBX3','CXDX3'],'Y3': ['BYCY3','DYAY3']}
-#         mergeX = {'X':['X2','Y3'],'Y':['Y2','X3']}
-#         new_X = add_pairs(new_X, **directXY)/2
-#         new_X = add_pairs(new_X, **mergeX)/2
-    return new_X
+    return new_X, new_consts 
 
 def norm_freq(x, freq_bias): 
     x['freq'] = x['freq'] + freq_bias
